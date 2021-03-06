0d41a5201ecc27f9131639d61c201142dd5e20d6
TheBlitzCode <66223603+FlxMeIdk@users.noreply.github.com>
2021-03-06 23:28:09 +0100

    Create Logger.php
    creaccion del logger para enviar mensajes a la consola

---
+++ src/lunity/utils/Logger.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace lunity\utils;
+
+class Logger {
+  
+  
+  public function inf($info) {
+    echo "[ Info ]: " . $info;
+  }
+  
+  public function debug($debug) {
+    echo "[ Info ]: " . $debug;
+  }
+  
+  public function inf($error) {
+    echo "[ Info ]: " . $error;
+  }
+
+
+?>
